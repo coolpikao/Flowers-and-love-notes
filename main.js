0edcc188b22ebb8008901faf994fad5a0e52d661
coolpikao <oscaritobencosme@gmail.com>
2026-08-02 19:55:39 -0400

    Adjust scroll duration and properties for mobile view

--- main.js
+++ main.js
@@ -23,12 +23,12 @@ window.addEventListener("DOMContentLoaded", () => {
     const isMobile = window.matchMedia('(max-width: 768px)').matches;
     const startDelay = 10000;
     const letterDelay = 1000;
-    const scrollDuration = isMobile ? 60000 : 50000;
+    const scrollDuration = isMobile ? 120000 : 60000;
     const fadeDuration = 3000;
 
     loveLetter.style.setProperty('--scroll-duration', `${scrollDuration / 1000}s`);
-    loveLetter.style.setProperty('--scroll-start', isMobile ? '5%' : '18%');
-    loveLetter.style.setProperty('--scroll-end', isMobile ? '-55%' : '-72%');
+    loveLetter.style.setProperty('--scroll-start', isMobile ? '58%' : '58%');
+    loveLetter.style.setProperty('--scroll-end', isMobile ? '-55%' : '-68%');
 
     flowers.classList.remove('fade-out');
     textContainer.classList.remove('visible');
